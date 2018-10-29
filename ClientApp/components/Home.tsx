5fad88b6bfe695a4e73cecb89a68d9a7fb4b09ad
Travis Wylie <travis@streamspike.com>
2018-10-30 00:15:08 +1300

    added divs to home page

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -5,23 +5,27 @@ import TextLoop from 'react-text-loop';
 export class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return <div>
-            <h1>Travis Wylie</h1>
-            <hr />
-            <h2>
-                <TextLoop>
-                    <span>Software Developer</span>
-                    <span>Student</span>
-                    <span>Web Developer</span>
-                </TextLoop>
-            </h2>
-            <h1>Hello, world!</h1>
-            <p>Welcome to your new single-page application, built with:</p>
-            <ul>
-                <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-                <li><a href='https://facebook.github.io/react/'>React</a> and <a href='http://www.typescriptlang.org/'>TypeScript</a> for client-side code</li>
-                <li><a href='https://webpack.github.io/'>Webpack</a> for building and bundling client-side resources</li>
-                <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-            </ul>
-        </div>;
+            <div className="container col-md-8">
+                <h1>Travis Wylie</h1>
+                <hr />
+                <div>
+                    <h2>
+                        <TextLoop>
+                            <span>Software Developer</span>
+                            <span>Student</span>
+                            <span>Web Developer</span>
+                        </TextLoop>
+                    </h2>
+                </div>
+                <h1>Hello, world!</h1>
+                <p>Welcome to your new single-page application, built with:</p>
+                <ul>
+                    <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
+                    <li><a href='https://facebook.github.io/react/'>React</a> and <a href='http://www.typescriptlang.org/'>TypeScript</a> for client-side code</li>
+                    <li><a href='https://webpack.github.io/'>Webpack</a> for building and bundling client-side resources</li>
+                    <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+                </ul>
+            </div>;
+        </div>
     }
 }

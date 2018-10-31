1611ac43b071468adf43c008dfd83cf14a7ea3b5
Travis Wylie <travis@streamspike.com>
2018-11-01 08:55:31 +1300

    added reference to config file added config file to gitignore added reference to config file in readme

--- Models/ContactModel.cs
+++ Models/ContactModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -13,8 +15,17 @@ namespace WebCv.Models
         public string Subject { get; set; }
         public string Content { get; set; }
     }
+
+    public class EmailModel
+    {
+        public string Email { get; set; }
+        public string EmailPassword { get; set; }
+        public string MailServer { get; set; }
+    }
+
     public class Contact
     {
+        public static EmailModel email;
         public static SmtpClient smtpClient;
         public static int SendEmail(ContactModel details)
         {
@@ -23,7 +34,7 @@ namespace WebCv.Models
                 MailMessage mail = new MailMessage();
 
                 //Setting From , To and CC
-                mail.From = new MailAddress("travis@streamspike.com", details.Name);
+                mail.From = new MailAddress(email.Email, details.Name);
                 mail.To.Add("traviswylie58@gmail.com");
                 mail.Subject = details.Subject;
                 mail.Body = $"From: {details.Email} \n {details.Content}";
@@ -38,19 +49,31 @@ namespace WebCv.Models
                 throw;
             }
         }
+
+        //start SMTP client at start of program to save connection time with each contact form filled
         public static void startSmtp()
         {
-            string password = "placeholder"; //TODO: add password config file (add file to gitignore)
-
-            if (smtpClient == null)
-            {
-                smtpClient = new SmtpClient("streamspike.com", 587);
-            }
-
-            smtpClient.Credentials = new System.Net.NetworkCredential("travis@streamspike.com", password); 
+            ConfigureEmail();
+            
+            smtpClient = new SmtpClient(email.MailServer, 587);
+            
+            smtpClient.Credentials = new System.Net.NetworkCredential(email.Email, email.EmailPassword); 
             smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
             smtpClient.EnableSsl = true;
             smtpClient.Timeout = 1000000;
+        }
+        public static void ConfigureEmail()
+        {
+            string file;
+            
+            file = Path.Combine(AppContext.BaseDirectory, "_config.json");
+
+            if (!File.Exists(file))
+            {
+                throw new ApplicationException("Unable to locate the _config.json file.");
+            }
+
+            email = JsonConvert.DeserializeObject<EmailModel>(File.ReadAllText(file));
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Travis-CV
 My website CV
+
+This website uses my Stream Spike web server in order to contact me with any messages put into the contact form. 
+Due to this requiring authentification I have stored the password for my email in a json file that I am not tracking via git.
+
+In order to start an instance of this website, you must first add a file named '_config.json' into the root directory of the code with the fields: 
+-'Email' with a value of your email
+-'EmailPassword' with a value of your emails password
+-'MailServer' with a value of your emails smtp server link/ip

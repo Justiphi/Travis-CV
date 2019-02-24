e3abfb8e1d98e3a4bd592dc04ea898f75daa459d
Travis Wylie <traviswylie58@gmail.com>
2019-02-25 08:15:54 +1300

    updated study

--- ClientApp/components/Education.tsx
+++ ClientApp/components/Education.tsx
@@ -5,6 +5,37 @@ export class Education extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return <div>
             <div className="col-md-8">
+            <h2> Currently Studying: </h2>
+                <div className='table-responsive'>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>
+                                    Qualification
+                                </th>
+                                <th>
+                                    Studying at
+                                </th>
+                                <th>
+                                    Year
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    BSc Major in Applied Computing
+                                </td>
+                                <td>
+                                    University of Waikato
+                                </td>
+                                <td>
+                                    2019
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
                 <h2> Qualifications: </h2>
                 <div className='table-responsive'>
                     <table className='table'>
